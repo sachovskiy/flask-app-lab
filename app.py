3fb92bedd875790845a0ef1ab7e011969808b0b9
sasasacovskij <oleksandr.sachovskyi.22@pnu.edu.ua>
2024-10-15 17:24:34 +0300

    версія 1

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return 'Hello, worldd'
+
+@app.route('/homepage')
+def home():
+    """View for the Home page of your website."""
+    return f"This is your homepage :) "
+
+
 if  __name__ == '__main__':
     app.run(debug=True)
