4701c17cdca1c2c4f67c45fb65644ddd0d3d26e6
sasasacovskij <oleksandr.sachovskyi.22@pnu.edu.ua>
2024-10-29 13:00:24 +0200

    Виправлений код

--- app/views.py
+++ app/views.py
@@ -1,31 +1,23 @@
-from flask import request, redirect, url_for, render_template, abort
+from flask import request, redirect, url_for, render_template
 from . import app
 
 @app.route('/')
 def main():
-    agent = request.user_agent
-    return render_template("home.html", agent=agent)
+    return render_template("base.html")
 
-
-@app.route('/homepage') 
+@app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
-
     return render_template("home.html", agent=agent)
 
-#users
-
+# Users
 @app.route("/hi/<string:name>")   #/hi/ivan?age=45
 def greetings(name):
     name = name.upper()
-    age = request.args.get("age", None, int)   
-
-    return render_template("hi.html", 
-                           name=name, age=age)
+    age = request.args.get("age", None, int)
+    return render_template("hi.html", name=name, age=age)
 
 @app.route("/admin")
 def admin():
-    to_url = url_for("greetings", name="administrator") + "?age=25"
-    print(to_url)
-    return redirect(to_url)
+    return redirect(url_for("greetings", name="administrator", age=25, _external=True))
