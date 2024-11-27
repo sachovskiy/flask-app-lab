62eb0a48f873627e78b8698a7aef9fa0308a8aee
sasasacovskij <oleksandr.sachovskyi.22@pnu.edu.ua>
2024-11-27 11:27:40 +0200

    lab6.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
 
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
+def create_app(config_name="config"):
+    app = Flask(__name__)
+    app.config.from_object(config_name)
 
-from . import views
+    with app.app_context():
+        from . import views
+        from .posts import post_bp
+        from .users import bp as user_bp
+        app.register_blueprint(post_bp)
+        app.register_blueprint(user_bp, url_prefix="/users")
 
-from .posts import post_bp
-from .users import bp as user_bp
-app.register_blueprint(post_bp)
-app.register_blueprint(user_bp, url_prefix="/users")
+    return app
